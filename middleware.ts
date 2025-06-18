eea64449e61667aaa2d58b2f05ef28243f33b06e
Manasd007 <manas.dubey007@gmail.com>
2025-06-18 13:21:24 +0530

    Fix Next.js 15 compatibility: Upgrade Clerk to v6.22.0 and update ESLint config

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,12 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/plans/:planId/community-plan(.*)", "/community-plans"],
+  publicRoutes: [
+    "/",
+    "/plans/:planId/community-plan(.*)",
+    "/community-plans",
+    "/api/get-token",
+  ],
 });
 
 export const config = {
